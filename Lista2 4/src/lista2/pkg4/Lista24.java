848d33776ab0bf03a6cf4e231e8263a07943e495
KlintBzl <klintscouts@gmail.com>
2025-09-03 14:30:48 -0300

    quarto exercicio

--- Lista2 4/src/lista2/pkg4/Lista24.java
+++ Lista2 4/src/lista2/pkg4/Lista24.java
@@ -6,7 +6,9 @@ public class Lista24 {
     public static void main(String[] args) {
         Livro l1 = new Livro("Jurassick Park", "Michael Crichton", 300, 60);
         l1.emprestar(45);
+        l1.emprestar(450);
         l1.Devolver(20);
+        l1.Devolver(200);
     }
     
 }
--- Lista2 4/src/lista2/pkg4/Livro.java
+++ Lista2 4/src/lista2/pkg4/Livro.java
@@ -19,13 +19,17 @@ public class Livro {
         if(NE > q){
             NE -= q;
             
+            System.out.println("Números de livros cadastrados no total: " + NI);
+            System.out.println("Numero de livros pedidos emprestados: " + q);
             System.out.println("Números de livros em estoque: " + NE);
-            System.out.println("Números de livros emprestados no total: " + NI);
+            System.out.println("");
             
         }else if(NE == 0){
             System.out.println("Infelizmente o livro '" + titulo + "' esgotou de estoque");
+            System.out.println("");
         }else if(NE < q){
             System.out.println("O livro '" + titulo + "' não tem em estoque a quantidade pedida");
+            System.out.println("");
         }
     }
     
@@ -33,12 +37,15 @@ public class Livro {
         int NF = NE + q;
         if( NF > NI){
             System.out.println("Opa! Tem certeza que está devolvendo o número de livros correto?");
+            System.out.println("");
         }else if(NF <= NI){
             NE += q;
             if(q == 1){
                 System.out.println("Tudo certinho, livro devolvido. Obrigado :D");
+                System.out.println("");
             }else{
                 System.out.println("Tudo certinho, livros devolvidos. Obrigado :D");
+                System.out.println("");
             }
             
             
